9d9a8acfeb1f52ac2a087418836c58d0368d5dde
Riovano <riovano82@gmail.com>
2021-08-10 19:00:23 +0700

    Done clear sketch, grid sizing, and grid visibility toggler

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
 const divsContainer = document.querySelector(".divs-container");
 const btnClear = document.getElementById("btn-clear");
-let divColumn = [];
-let divRow = [];
+const inputSquare = document.getElementById("input-square");
+const totalSquare = document.querySelector(".total-square");
+const eye = document.querySelectorAll(".eye");
 
-//Create 16x16 grid of square divs
-createDivs(16, 16);
-console.log(divColumn.length + " - " + divRow.length);
+btnClear.addEventListener("click", clearSketch);
+totalSquare.textContent = `${inputSquare.value}x${inputSquare.value}`;
+inputSquare.addEventListener("change", updateHandler);
+inputSquare.addEventListener("mousemove", () => {
+    totalSquare.textContent = `${inputSquare.value}x${inputSquare.value}`;
+});
+inputSquare.addEventListener("mouseup", clearSketch);
+window.addEventListener("load", updateHandler);
 
-function createDivs(row, column) {
-    for (let i = 0; i < column; i++) {
+eye.forEach((e) => {
+    e.addEventListener("click", isVisible);
+});
+
+function isVisible() {
+    let div = document.querySelectorAll(".game");
+    if (this.textContent === "visibility") {
+        this.textContent = "visibility_off";
+    } else {
+        this.textContent = "visibility";
+    }
+    div.forEach((e) => {
+        e.classList.toggle("active");
+    });
+}
+
+function updateHandler() {
+    removeDivs();
+    createDivs(inputSquare.value);
+    divsContainer.style.gridTemplateRows = `repeat(${inputSquare.value}, 1fr)`;
+    divsContainer.style.gridTemplateColumns = `repeat(${inputSquare.value}, 1fr)`;
+}
+
+function createDivs(number) {
+    for (let i = 0; i < number; i++) {
         const div = document.createElement("div");
+        let divColumn = [];
         divColumn[i] = divsContainer.appendChild(div);
         div.classList.add("game");
         div.addEventListener("mouseover", addTrail);
-        for (let j = 0; j < row; j++) {
+        for (let j = 1; j < number; j++) {
             const div = document.createElement("div");
+            let divRow = [];
             divRow[i] = divsContainer.appendChild(div);
             div.classList.add("game");
             div.addEventListener("mouseover", addTrail);
@@ -22,16 +53,19 @@ function createDivs(row, column) {
     }
 }
 
+function removeDivs() {
+    let div = document.querySelectorAll(".game");
+    div.forEach((e) => {
+        divsContainer.removeChild(e);
+    });
+}
+
 function addTrail() {
     this.style.background = "black";
 }
 
-//Clear Sketch
-btnClear.addEventListener("click", clearSketch);
-
 function clearSketch() {
     let div = document.querySelectorAll(".game");
-    // console.log(div);
     div.forEach((e) => {
         e.style.background = "white";
     });
